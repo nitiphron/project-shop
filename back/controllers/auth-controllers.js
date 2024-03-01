4905f2f273bd0fd80bdb36c198a088cb1834828f
nitiphron <hardandmin@gmail.com>
2024-03-01 10:34:56 +0700

    Initial commit

--- back/controllers/auth-controllers.js
+++ back/controllers/auth-controllers.js
@@ -1,62 +1,61 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const db = require('../models/db')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../models/db');
 
-module.exports.register = async (req, res,next) => {
-    const {username, password, email, first_name, last_name, address, phone } = req.body
+module.exports.register = async (req, res, next) => {
+    const { username, password, email, first_name, last_name, address, phone } = req.body;
     try {
         // validation
-    if( !(username && password && email && first_name && last_name && address && phone)) {
-        return next( new Error('Fulfill all inputs'))
-    }
-    const hashedPassword = await bcrypt.hash(password, 8)
-    console.log(hashedPassword)
-    const data = {
-        username,
-        password : hashedPassword,
-        email, 
-        first_name, 
-        last_name, 
-        address, 
-        phone 
-    }
+        if (!(username && password && email && first_name && last_name && address && phone)) {
+            return next(new Error('Fulfill all inputs'));
+        }
+        const hashedPassword = await bcrypt.hash(password, 8);
+        const data = {
+            username,
+            password: hashedPassword,
+            email,
+            first_name,
+            last_name,
+            address,
+            phone: parseInt(phone) // Convert phone to integer
+        };
 
-    const rs = await db.user.create({ data })
-    console.log(rs)
+        // Create user in database
+        const newUser = await db.user.create({
+            data
+        });
 
-    res.json({ msg: 'Register successful'})
-    } catch(err){
-        next(err)
+        res.json({ msg: 'Register successful', user: newUser });
+    } catch (err) {
+        next(err);
     }
-    
-    
-}
-module.exports.login = async (req, res, next) => {
-  const {username, password} = req.body
-  try {
-    // validation
-    if( !(username.trim() && password.trim()) ) {
-      throw new Error('username or password must not blank')
-    }
-    // find username in db.user
-    const user = await db.user.findFirstOrThrow({ where : { username }})
-    // check password
-    const pwOk = await bcrypt.compare(password, user.password)
-    if(!pwOk) {
-      throw new Error('invalid login')
-    }
-    // issue jwt token 
-    const payload = { id: user.id }
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: '30d'
-    })
-    console.log(token)
-    res.json({token : token})
-  }catch(err) {
-    next(err)
-  }
 };
 
-exports.getme = (req,res,next) => {
-  res.json(req.user)
-}
+module.exports.login = async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+        // validation
+        if (!(username.trim() && password.trim())) {
+            throw new Error('Username or password must not be blank');
+        }
+        // find username in db.user
+        const user = await db.user.findFirstOrThrow({ where: { username } });
+        // check password
+        const pwOk = await bcrypt.compare(password, user.password);
+        if (!pwOk) {
+            throw new Error('Invalid login');
+        }
+        // issue jwt token 
+        const payload = { id: user.id };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: '30d'
+        });
+        res.json({ token });
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.getme = (req, res, next) => {
+    res.json(req.user);
+};
--- front/src/layout/RegisterForm.jsx
+++ front/src/layout/RegisterForm.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+
 
 export default function RegisterForm() {
   const [input, setInput] = useState({
